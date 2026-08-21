e98794f56d6e754f1c8018c2405c11d1beec1eeb
Ashutosh Banerjee <ashutoshbanerjee939@gmail.com>
2026-08-21 11:01:14 +0530

    notes for variable is added

---
+++ chapter01/variable.py
@@ -0,0 +1,30 @@
+lenght=11
+username="vir"
+score=90
+temp=26.7
+
+
+# user name="avir"  not allowed
+# 06user="avir" not allowed
+# $#$score=9 not allowed
+# if = true  not aloowed bcoz of key workds
+
+print(lenght)
+print(username)
+print(score)
+
+
+# print value and string
+print("lenght of the rectangle is ",lenght)
+print("usrname is",username)
+
+
+# perfrorming operations using varibale and basic operations
+# find area of rectangle
+l=9
+b=18
+print(l*b)
+print("area of rectangle is",l*b)
+area=l*b # using varibale l and b we are wrinting value 9 and 18
+print("area of rectangle is",area)
+
