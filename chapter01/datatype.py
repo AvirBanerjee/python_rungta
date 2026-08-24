a23f4821497e788bfb09d6547798e4e27ef91661
Ashutosh Banerjee <ashutoshbanerjee939@gmail.com>
2026-08-24 13:59:13 +0530

    datatype code is added

---
+++ chapter01/datatype.py
@@ -0,0 +1,24 @@
+lenght=8
+temp=34.6
+username="avir"
+
+#int -> interger 
+a=9
+b=676767
+
+# float
+temp=9.7
+pi=3.14
+
+# string 
+#sring is set of char
+# 1
+# 2
+
+name="ashutosh banerjee"
+mystr2="""helooo write code inspire others 
+I am ashutosh
+I am a senior tecnical trainer"""
+print(name)
+
+print(mystr2)
--- chapter01/variable.py
+++ chapter01/variable.py
@@ -9,22 +9,42 @@ temp=26.7
 # $#$score=9 not allowed
 # if = true  not aloowed bcoz of key workds
 
-print(lenght)
-print(username)
-print(score)
+# print(lenght)
+# print(username)
+# print(score)
 
 
 # print value and string
-print("lenght of the rectangle is ",lenght)
-print("usrname is",username)
+# print("lenght of the rectangle is ",lenght)
+# print("usrname is",username)
 
 
 # perfrorming operations using varibale and basic operations
 # find area of rectangle
-l=9
-b=18
-print(l*b)
-print("area of rectangle is",l*b)
-area=l*b # using varibale l and b we are wrinting value 9 and 18
-print("area of rectangle is",area)
+# l=9
+# b=18
+# print(l*b)
+# print("area of rectangle is",l*b)
+# area=l*b # using varibale l and b we are wrinting value 9 and 18
+# print("area of rectangle is",area)
 
+# a=5 and b=7 how will u swap.
+# a=5
+# b=7
+# print("prev value of a is",a)
+# print("prev value of b is",b)
+# c=a
+# a=b
+# b=c
+# print(" value of a is",a)
+# print(" value of b is",b)
+
+# print a string name ="vir" in a sentence " MY name is _______ .. and I am learning coding"
+name="your name"
+age=15
+umar="15"
+# print("my name is",name," i am here to code")
+# print(f"my name is {name} i am here to code and my age is {age}")
+
+print(age)
+print(umar)
