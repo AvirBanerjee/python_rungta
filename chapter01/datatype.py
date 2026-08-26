405b041f4dbf9890e67d1f810de6956862a10fd5
Ashutosh Banerjee <ashutoshbanerjee939@gmail.com>
2026-08-26 10:46:27 +0530

    datatype added

--- chapter01/datatype.py
+++ chapter01/datatype.py
@@ -10,15 +10,38 @@ b=676767
 temp=9.7
 pi=3.14
 
+# complex
+num=3+4j # imagine 
+
+
 # string 
 #sring is set of char
-# 1
-# 2
+# """heloooo""" are user for multi-line string(multiline comments)
 
 name="ashutosh banerjee"
 mystr2="""helooo write code inspire others 
 I am ashutosh
 I am a senior tecnical trainer"""
-print(name)
+# print(name)
+# print(mystr2)
 
-print(mystr2)
+
+# bool 
+
+isloggedIn=False
+loggedIn=True
+
+# None
+temp=None # NO DATA BUT ALLOCATION
+
+
+number=10
+Temperature=45.7
+scoreCurrent="10"
+username="vir"
+logged=True
+print(type(number)) #10
+print(type(Temperature)) #<class float>
+print(scoreCurrent) #10
+print(username)
+print(type(logged))
