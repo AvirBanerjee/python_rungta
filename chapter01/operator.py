25dd50e50e50d8ad83aa86838e5cef0835e2948c
Ashutosh Banerjee <ashutoshbanerjee939@gmail.com>
2026-08-27 10:50:29 +0530

    operator code added

---
+++ chapter01/operator.py
@@ -0,0 +1,30 @@
+#Arithmetic Operators
+a=17
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
+
+#Assignment Operators
+a+=6 # a = a+6
+a-=6 # a = a-6
+a/=6 # a = a/6
+a//=6 # a = a//6
+a*=6 # a = a*6
+a**=6 # a = a**6
+a%=6 # a = a%6
+
+#Comparison Operators
+a=6
+# b=7
+b=6
+print(a==b) # True
+print(a!=b) # True
+print(a<b) #  False
+print(a>b) #  False
+print(a<=b) # True  
+print(a>=b) # True  
