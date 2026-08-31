56c63614383cb8fcafd293f413de802fe8cb40f1
Ashutosh Banerjee <ashutoshbanerjee939@gmail.com>
2026-08-31 14:20:48 +0530

    logical operator code added

--- chapter01/operator.py
+++ chapter01/operator.py
@@ -1,13 +1,13 @@
 #Arithmetic Operators
 a=17
 b=5
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a%b)
-print(a//b)
-print(a**b)
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a//b)
+# print(a**b)
 
 #Assignment Operators
 a+=6 # a = a+6
@@ -22,9 +22,55 @@ a%=6 # a = a%6
 a=6
 # b=7
 b=6
-print(a==b) # True
-print(a!=b) # True
-print(a<b) #  False
-print(a>b) #  False
-print(a<=b) # True  
-print(a>=b) # True  
+# print(a==b) # True
+# print(a!=b) # True
+# print(a<b) #  False
+# print(a>b) #  False
+# print(a<=b) # True  
+# print(a>=b) # True  
+
+# Logical Operators
+# leave is there or not
+
+# isHoliday=True
+# isWeekend=False
+# print(isHoliday and isWeekend)
+
+# user1_age=120
+# # user2_age=16
+# print("Eligible ot vote : ")
+# print(user1_age > 18 and user1_age < 100) 
+# and -> both conditions true
+
+# shinchan=5
+# mima=3
+
+# eligible_lower=2
+# eligible_higher=10
+
+# print("Eligible to ride:")
+# print(shinchan <= eligible_higher and mima <=  eligible_lower - 1)
+
+# ice_cream1="Coco Bar"
+# ice_cream2="kasata"
+# ice_cream3="butter scotch"
+
+# wish_1="kasata"
+# # wish_2="Cornaco"
+
+# print("Impressed:")
+# print(wish_1==ice_cream1 or wish_1==ice_cream2)
+# # or -> true if any one condition is true
+
+# Grading system Pass and fail
+# score=90
+# MArksOF5=79
+# MArksOF6=74
+# print(MArksOF6 > 70 or MArksOF5 > 80) # 
+
+# NOT 
+# Mein_accha_insaan_hun=False
+# print(not Mein_accha_insaan_hun)
+a=6
+b=8
+print(not a<=b)
