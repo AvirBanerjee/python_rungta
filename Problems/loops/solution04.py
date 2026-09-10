9e07ef0e8aa6862ac069d2dbdcaa13b1c2116af1
Ashutosh Banerjee <ashutoshbanerjee939@gmail.com>
2026-09-10 15:50:26 +0530

    more solution for loops added

---
+++ Problems/loops/solution04.py
@@ -0,0 +1,30 @@
+# sum of n natural no.
+# n=10
+# total=0
+# for i in range(1,n+1):
+#     total=total+i
+#     print(f"value of total is {total} in interation number {i}")
+
+# odd or even
+# n=20
+# sum_even=0
+# for i in range(1,n+1):
+#     if i%2==0:
+#         sum_even=sum_even+i 
+# print("sum of even numbers is",sum_even)        
+# sum_odd=0          
+# for i in range(1,n+1):
+#     if i%2!=0:
+#         sum_odd=sum_odd+i  
+# print("sum of odd numbers is",sum_odd)        
+
+# # factorial
+# n=5
+# fact=1
+# for i in range(1,n+1):
+#     fact*=i # fact = fact *i
+#     print(f"factorial value at {i} interation is {fact}")
+# print(f"final value is {fact}")    
+
+# Table
+n=5
