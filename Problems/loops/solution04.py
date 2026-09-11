9056f0905cf370f4472074029b5a9d0402df704d
Ashutosh Banerjee <ashutoshbanerjee939@gmail.com>
2026-09-11 15:32:00 +0530

    solutions for questions on factor and prime number

--- Problems/loops/solution04.py
+++ Problems/loops/solution04.py
@@ -26,5 +26,16 @@
 #     print(f"factorial value at {i} interation is {fact}")
 # print(f"final value is {fact}")    
 
-# Table
-n=5
+
+#factor
+n=6
+total=0
+for i in range(1,n):
+    if n%i==0:
+        total+=i
+        print(f"In place factor number {i} value of total is {total}")
+if total==n:
+    print("perfect number")
+else:
+    print("Not perfect number")        
+
