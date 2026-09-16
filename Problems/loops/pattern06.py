7474d48f3c7f692cf975d76a22202d6c2f876376
Ashutosh Banerjee <ashutoshbanerjee939@gmail.com>
2026-09-16 13:55:56 +0530

    Last question of pattern is added

---
+++ Problems/loops/pattern06.py
@@ -0,0 +1,13 @@
+n = 5
+
+for i in range(1, n + 1):
+    # spaces
+    for j in range(n - i):
+        print(" ", end="")
+    # increasing numbers
+    for j in range(1, i + 1):
+        print(j, end="")
+    #Decreasing numbers
+    for j in range(i - 1, 0, -1):
+        print(j, end="")
+    print()
