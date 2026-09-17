3ff6743235aba7e7f6c06533819cf608527bd04f
Ashutosh Banerjee <ashutoshbanerjee939@gmail.com>
2026-09-17 11:05:21 +0530

    questions for while loop added

--- Problems/loops/pattern06.py
+++ Problems/loops/pattern06.py
@@ -5,9 +5,15 @@ for i in range(1, n + 1):
     for j in range(n - i):
         print(" ", end="")
     # increasing numbers
-    for j in range(1, i + 1):
-        print(j, end="")
-    #Decreasing numbers
-    for j in range(i - 1, 0, -1):
-        print(j, end="")
-    print()
+    for j in range(2*i-1):
+        print("*", end="")
+    print()    
+for i in range(n,0,-1):
+    # spaces
+    for j in range(n - i):
+        print(" ", end="")
+    # increasing numbers
+    for j in range(2*i-1):
+        print("*", end="")
+    print()     
+    
---
+++ chapter01/whileloop.py
@@ -0,0 +1,17 @@
+# n=5
+# i=0
+# while i<n:
+#     print(i)
+#     i=i+1
+
+# n=5
+# i=1
+# while i<=10:
+#     print(f"{n} X {i} = {n*i}")
+#     i+=1
+
+n=10
+i=10
+while i>=0:
+    print(i)
+    i-=1
