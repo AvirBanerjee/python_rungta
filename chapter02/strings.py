872eee3a1405a333d915cf1e5242b0d59fd35f91
Ashutosh Banerjee <ashutoshbanerjee939@gmail.com>
2026-09-21 14:23:27 +0530

    string stared discused till indexing and slicing

---
+++ chapter02/strings.py
@@ -0,0 +1,15 @@
+# name="avir"
+# name2='Ashutosh'
+# name="""Vir"""
+# sentence="""I AM Following ASHUTOSH sir's
+# jnvowdvjweovnwejnovwniovnownkonwovnownvownvownvo"""
+
+# firstname="Ashutosh"
+# lastname="banerjee"
+# fullname=firstname +" "+lastname
+# print(fullname*3)
+
+sentence="Ashutosh"
+# print(sentence)
+print(sentence[-1:-6:-1])
+
