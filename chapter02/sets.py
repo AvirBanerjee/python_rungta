aa54bae6f1e399c60619ea1bde30bc247f8d4c5d
Ashutosh Banerjee <ashutoshbanerjee939@gmail.com>
2026-09-25 11:09:04 +0530

    sets added to the code base

---
+++ chapter02/sets.py
@@ -0,0 +1,25 @@
+# info=set() ->set
+# info={} -> Dict 
+# name="nitin"
+info={1,2,3,3,2,"avir",26.7}
+# marks=[76,34,56,43]
+# score=(23,34,56,43)
+# # info.add(marks) # -> {1,2,3,4,[],98}
+# info.add(score)
+# name="vikas"
+# info.add(name)
+# info.update(marks)# -> {1,2,3,4,23,34,56,98}
+# info.clear()
+
+
+# info.discard(99)
+# print(info)
+
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+print(A.union(B))                   
+print(A.intersection(B))            
+print(A.difference(B))              
+print(A.symmetric_difference(B))    
+
